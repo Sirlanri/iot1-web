8ff12a605bf2b4d12199b3f47453a4b9a3ed4031
深蓝 <mail@ri-co.cn>
2021-04-21 09:56:35 +0800

    创建echarts 温湿度图

--- src/views/temphumi.vue
+++ src/views/temphumi.vue
@@ -5,31 +5,190 @@
         <v-switch v-model="freshBtn" label="数据开关"></v-switch>
       </v-col>
       <v-col>
-        <v-card-text>
-          每秒刷新频率：{{freshFre}}
-        </v-card-text>
+        <v-card-text> 每秒刷新频率：{{ freshFre }} </v-card-text>
         <v-slider v-model="freshFre" max="10"></v-slider>
       </v-col>
     </v-row>
 
     <v-row>
-      
+      <div ref="temphumi" class="temp-humi-chart"></div>
     </v-row>
   </div>
 </template>
 
 <script>
-export default {
-  data(){
-    return{
-      freshBtn:false,
-      freshFre:1,
+// 引入 echarts 核心模块，核心模块提供了 echarts 使用必须要的接口。
+import * as echarts from "echarts/core";
+// 引入柱状图图表，图表后缀都为 Chart
+import { BarChart, LineChart } from "echarts/charts";
 
-    }
-  }
-}
+// 引入 Canvas 渲染器，注意引入 CanvasRenderer 或者 SVGRenderer 是必须的一步
+import { CanvasRenderer } from "echarts/renderers";
+//引入组件
+import {
+  TitleComponent,
+  ToolboxComponent,
+  TooltipComponent,
+  GridComponent,
+  LegendComponent,
+  DataZoomComponent,
+} from "echarts/components";
+
+// 注册必须的组件
+echarts.use([
+  TitleComponent,
+  ToolboxComponent,
+  TooltipComponent,
+  GridComponent,
+  LegendComponent,
+  DataZoomComponent,
+  BarChart,
+  LineChart,
+  CanvasRenderer,
+]);
+export default {
+  data() {
+    return {
+      freshBtn: false,
+      freshFre: 1,
+      tempRes:[20,21,24,26,30,27,30],
+      humiRes:[10,50,60,90,80,70,60],
+      
+    };
+  },
+
+  mounted(){
+    this.draw()
+  },
+
+  computed:{
+    tempData(){
+      var data=[]
+      if (this.tempRes.length<=15) {
+        data=this.tempRes
+      }else{
+        data=this.tempRes.slice(-15)
+      }
+      return data
+    },
+
+    humiData(){
+      var data=[]
+      if (this.humiRes.length<=15) {
+        data=this.humiRes
+      }else{
+        data=this.humiRes.slice(-15)
+      }
+      return data
+    },
+
+    maxHumi(){
+      return 100
+    },
+    maxTemp(){
+      return Math.max.apply(null,this.tempRes)+10
+    },
+  },
+
+
+  methods: {
+    draw() {
+      var dom = this.$refs.temphumi;
+      var chart = echarts.init(dom);
+      chart.setOption({
+        title: {
+          text: "动态数据",
+          subtext: "纯属虚构",
+        },
+        tooltip: {
+          trigger: "axis",
+          axisPointer: {
+            type: "cross",
+            label: {
+              backgroundColor: "#283b56",
+            },
+          },
+        },
+        legend: {
+          data: ["潮湿度", "温度"],
+        },
+        toolbox: {
+          show: true,
+          feature: {
+            dataView: { readOnly: false },
+            restore: {},
+            saveAsImage: {},
+          },
+        },
+        dataZoom: {
+          show: false,
+          start: 0,
+          end: 100,
+        },
+        xAxis: [
+          {
+            type: "category",
+            boundaryGap: true,
+            data:(function (){
+                var now = new Date();
+                var res = [];
+                var len = 15;
+                while (len--) {
+                    res.unshift(now.toLocaleTimeString().replace(/^\D*/,''));
+                    now = new Date(now - 2000);
+                }
+                return res;
+            })()
+          },
+          
+        ],
+        yAxis: [
+          {
+            type: "value",
+            scale: true,
+            name: "潮湿度",
+            max: this.maxHumi,
+            min: 0,
+            axisLabel:{
+              formatter:'{value} %'
+            },
+            boundaryGap: [0.2, 0.2],
+          },
+          {
+            type: "value",
+            scale: true,
+            name: "温度",
+            max: this.maxTemp,
+            min: 0,
+            axisLabel:{
+              formatter:'{value} ℃'
+            },
+            boundaryGap: [0.2, 0.2],
+          },
+        ],
+        series: [
+          {
+            name: "潮湿度",
+            type: "bar",
+            xAxisIndex: 0,
+            //yAxisIndex: 1,
+            data: this.humiData
+          },
+          {
+            name: "温度",
+            type: "line",
+            data: this.tempData
+          },
+        ],
+      });
+    },
+  },
+};
 </script>
 
 <style>
-
+.temp-humi-chart{
+  height: 500px;
+  width: 100%;
+}
 </style>
