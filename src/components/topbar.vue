fc6e5318437f8a3185ee531847fb77958144d91f
深蓝 <mail@ri-co.cn>
2021-05-08 08:54:27 +0800

    顶栏固定+透明

--- src/components/topbar.vue
+++ src/components/topbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app-bar>
+  <v-app-bar fixed class="top-bar" color="rgba(255,255,255,0.7)">
     <v-app-bar-nav-icon></v-app-bar-nav-icon>
     <v-app-bar-title> 深蓝 树莓派室温检测 </v-app-bar-title>
   </v-app-bar>
@@ -13,4 +13,5 @@ export default {
 </script>
 
 <style>
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <div class="home-back"></div>
     <topbar></topbar>
+    <div class="top-space"></div>
     <v-row>
       <v-col lg="10" offset-lg="1" cols="12" offset="0">
         <router-view></router-view>
@@ -30,8 +31,10 @@ export default {
   background-image: url("https://cdn.ri-co.cn/project/iot1/sunny1.jpg");
   background-size: cover;
   background-repeat: no-repeat;
- 
 	background-position: center;
   opacity: 0.7;
+}
+.top-space{
+  height: 4rem;
 }
 </style>
